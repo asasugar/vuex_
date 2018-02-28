b0e8309a3ea7343caae549e0eb3c416e34afbc19
xxj95719 <87416959@qq.com>
2018-02-28 11:34:31 +0800

    add vconsole

--- src/components/VuexDemo.vue
+++ src/components/VuexDemo.vue
@@ -14,6 +14,7 @@
 </template>
 
 <script>
+
   import {
     mapGetters,
     mapActions
--- src/main.js
+++ src/main.js
@@ -4,9 +4,23 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store/index'
-
+import VConsole from 'vconsole'
 Vue.config.productionTip = false
 /* eslint-disable no-new */
+
+// 初始化vConsole
+window.vConsole = new VConsole({
+  defaultPlugins: ['system', 'network', 'element', 'storage'], // 可以在此设定要默认加载的面板
+  maxLogNumber: 1000,
+  onReady: function() {
+    // console.log('vConsole is ready.');
+  },
+  onClearLog: function() {
+    // console.log('on clearLog');
+  }
+});
+
+console.info('欢迎使用 vConsole。vConsole 是一个由微信公众平台前端团队研发的 Web 前端开发者面板，可用于展示 console 日志，方便开发、调试。');
 new Vue({
   el: '#app',
   router,
